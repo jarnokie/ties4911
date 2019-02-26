1a4ab52af0d38fe1e36f5ea2fe5ab3e1bdb328c7
Jarno <jarnokie@gmail.com>
2019-02-26 16:56:42 +0200

    Show watson keywords, use bootstrap

--- webapp.py
+++ webapp.py
@@ -10,12 +10,12 @@ nlu = watson_requests.make_nlu(api_key)
 #url = "https://www.nytimes.com/2019/02/25/movies/oscars-moments-best-worst.html"
 
 def make_html(json):
-    elems = []
+    keywords = []
     for key in json.keys():
         if key == "keywords":
             for keyword in json[key]:
-                elems.append("%s, relevance %.2f" % (keyword["text"], keyword["relevance"]))
-    return elems
+                keywords.append((keyword["text"], keyword["relevance"]))
+    return keywords
 
 @app.route("/", methods=["GET"])
 def main_page():
@@ -26,6 +26,6 @@ def main_post():
     url = request.form["url"]
     json_result = watson_requests.analyze(nlu, url)
     s = make_html(json_result)
-    return render_template("index.html", content=s)
+    return render_template("index.html", watson_keywords=s, url=url)
 
 
