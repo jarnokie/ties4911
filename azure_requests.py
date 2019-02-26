b8384579b552a4fa65051db0039aa772b0f3ae1e
Irene V <iiki@iki.fi>
2019-02-26 21:35:28 +0200

    Add Azure key phrase detection.

---
+++ azure_requests.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+
+# Strips tags from HTML, returning regular text.
+def strip_tags(html):
+    soup = BeautifulSoup(html, features="html.parser")
+    text = soup.get_text()
+    return text
+
+def read_url(url):
+    response = requests.get(url)
+    return strip_tags(response.content)
+
+
+def analyze(url, api_key):
+    content = read_url(url)
+    # Maximum length of document is 5000 characters
+    # https://docs.microsoft.com/bs-latn-ba/azure/cognitive-services/text-analytics/how-tos/text-analytics-how-to-keyword-extraction
+    content = str(content.replace("\n", "").encode("utf-8"))[0:5000]
+    headers = {
+        # Request headers
+        'Ocp-Apim-Subscription-Key': api_key
+    }
+    documents = { 'documents': [
+        { 'id': '1', 'language': 'en', 'text': content },
+    ]}
+    text_analytics_base_url = "https://northeurope.api.cognitive.microsoft.com/text/analytics/v2.0/"
+    key_phrase_api_url = text_analytics_base_url + "keyPhrases"
+    response  = requests.post(key_phrase_api_url, headers=headers, json=documents, timeout=30)
+    return response.json()
--- webapp.py
+++ webapp.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
+import configparser
 import json
 import watson_requests
 import google_requests
 import configparser
+import azure_requests
 
 app = Flask(__name__)
 
 config = configparser.ConfigParser()
 config.read('apikey.txt')
 watson_api_key = config.get('APIKEYS', 'WATSON')
+azure_api_key = config.get('APIKEYS', 'AZURE')
 
 nlu = watson_requests.make_nlu(watson_api_key)
 
@@ -32,6 +35,12 @@ def get_keywords_google(entities):
         keywords.append((e.name, e.salience))
     return keywords
 
+def get_keywords_azure(json):
+    keywords = []
+    if "documents" in json and len(json["documents"]) > 0 and "keyPhrases" in json["documents"][0]:
+        keywords = json["documents"][0]["keyPhrases"]
+    return [[k, i] for i, k in enumerate(keywords)]
+
 @app.route("/", methods=["GET"])
 def main_page():
     return render_template("index.html")
@@ -49,6 +58,10 @@ def main_post():
     e = google_requests.get_entities(google_client, url)
     g = get_keywords_google(e)
     
+    azure_response = azure_requests.analyze(url, azure_api_key)
+    azure_keywords = get_keywords_azure(azure_response)
+
     # render page
-    return render_template("index.html", watson_keywords=s, google_keywords=g, url=url)
+    return render_template("index.html", watson_keywords=s, google_keywords=g, \
+                           azure_keywords=azure_keywords, url=url)
 
