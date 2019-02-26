810690ae94162b47ae7a8f4634a4169b83de5dfa
Jarno <jarnokie@gmail.com>
2019-02-26 17:29:05 +0200

    Renamed func

--- webapp.py
+++ webapp.py
@@ -9,7 +9,10 @@ with open("apikey.txt", "r") as f:
 nlu = watson_requests.make_nlu(api_key)
 #url = "https://www.nytimes.com/2019/02/25/movies/oscars-moments-best-worst.html"
 
-def make_html(json):
+def get_keywords_watson(json):
+    """
+    Return list of (keywords, relevance from watson json result
+    """
     keywords = []
     for key in json.keys():
         if key == "keywords":
@@ -23,9 +26,13 @@ def main_page():
 
 @app.route("/", methods=["POST"])
 def main_post():
+    # get url from post form
     url = request.form["url"]
+    # Analyze url with watson
     json_result = watson_requests.analyze(nlu, url)
-    s = make_html(json_result)
+    # get keyword array from json
+    s = get_keywords_watson(json_result)
+    # render page
     return render_template("index.html", watson_keywords=s, url=url)
 
 
