fa0b2209fea3a06434325d70d5b0a9d8bf1df6ca
Irene V <iiki@iki.fi>
2019-02-26 18:13:19 +0200

    Add template for apikey-file.

--- webapp.py
+++ webapp.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request
 import json
 import watson_requests
+import configparser
 
 app = Flask(__name__)
 
-with open("apikey.txt", "r") as f:
-    api_key = f.read().strip()
-nlu = watson_requests.make_nlu(api_key)
+config = configparser.ConfigParser()
+config.read('apikey.txt')
+watson_api_key = config.get('APIKEYS', 'WATSON')
+
+nlu = watson_requests.make_nlu(watson_api_key)
 #url = "https://www.nytimes.com/2019/02/25/movies/oscars-moments-best-worst.html"
 
 def get_keywords_watson(json):
@@ -34,5 +37,4 @@ def main_post():
     s = get_keywords_watson(json_result)
     # render page
     return render_template("index.html", watson_keywords=s, url=url)
-
 
